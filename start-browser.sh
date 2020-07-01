78ba019c96d1e11459ab72ba479ce234a9a9b488
Stefan Eichenberger <stefan.eichenberger@toradex.com>
2020-07-01 09:59:59 +0200

    Dockerfile: add virtual keyboard extension
    Add the virtual keyboard extension for chromium. The keyboard can be
    started with the argument --virtual-keyboard.
    
    Signed-off-by: Stefan Eichenberger <stefan.eichenberger@toradex.com>

--- start-browser.sh
+++ start-browser.sh
@@ -6,6 +6,8 @@ url="www.toradex.com"
 # default parms for kiosk mode
 chromium_parms_base="--test-type --allow-insecure-localhost --disable-notifications --check-for-update-interval=315360000 "
 chromium_parms="--kiosk "
+# Additional params should be stacked chromium_parms_extended="$chromium_params_extended ..."
+chromium_parms_extended=""
 
 for arg in "$@"
 do
@@ -17,6 +19,12 @@ do
     --browser-mode)
         chromium_parms="--start-maximized "
         shift
+        ;;
+    --virtual-keyboard)
+        # Load the virtual keyboard
+        chromium_parms_extended="$chromium_parms_extended --load-extension=/chrome-extensions/chrome-virtual-keyboard"
+        shift
+        ;;
     esac
 done
 
@@ -24,4 +32,4 @@ if [ ! -z "$1" ]; then
     url=$1
 fi
 
-exec chromium $chromium_parms_base $chromium_parms$url
+exec chromium $chromium_parms_base $chromium_parms_extended $chromium_parms$url
