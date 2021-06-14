88491e1931db31c6b0839a492d8831897df1b042
Sergio Prado <sergio.prado@toradex.com>
2021-06-14 13:45:31 -0300

    Introduce Cog as an alternative web browser engine
    This commit adds Cog as an alternative to Chromium for a web browser
    engine in the kiosk-mode-browser container.
    
    Chromium is still the default browser engine used to open web pages,
    but one can change to Cog by passing '--env KIOSK_LAUNCHER=cog' to
    Docker when starting the container.
    
    Currently, due to the lack of support on non-GPU devices (e.g. iMX7),
    Cog is only installed on Wayland builds (iMX6 and iMX8).
    
    Chromium and Cog tested on Apalis iMX6 and Verdin iMX8MM. Chromium
    tested on Colibri iMX7.
    
    Related-to: TOR-1890
    
    Signed-off-by: Sergio Prado <sergio.prado@toradex.com>

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-# First stage download virtual keyboard
 ARG IMAGE_ARCH=linux/arm
 # For arm64 use:
 # ARG IMAGE_ARCH=linux/arm64
+
 ARG BASE_IMAGE=wayland-base
 # For iMX7 use:
 # ARG BASE_IMAGE=debian
+
 ARG IMAGE_TAG=2
 # For iMX7 use:
 # ARG IMAGE_TAG=2-bullseye
+
+# First stage download virtual keyboard
 FROM --platform=$IMAGE_ARCH torizon/$BASE_IMAGE:$IMAGE_TAG AS first
 RUN apt-get -y update && \
     apt-get install -y --no-install-recommends wget
@@ -25,14 +28,28 @@ ARG BUILD_TYPE=wayland
 # For iMX7 use:
 # ARG BUILD_TYPE=x11
 
+# If x11 build (iMX7), use Chromium from Debian feed
 RUN test "$BUILD_TYPE" = "x11" && \
         sed -i '/feeds.toradex.com/d' /etc/apt/sources.list || true
 
+# Install Chromium
 RUN apt-get -y update && \
     apt-get install -y --no-install-recommends chromium chromium-sandbox && \
     apt-get clean && apt-get autoremove && \
     update-mime-database /usr/share/mime && \
     rm -rf /var/lib/apt/lists/*
+
+# Install Cog on wayland builds (iMX6/iMX8)
+# Cog is installed from the experimental feed to get the latest version that has some fixes
+# for iMX based devices. Also, we are currently not installing Cog on x11 builds (iMX7) for
+# two main reasons: 1) install fails with a package dependency issue (it seems the reason
+# is that Chromium installed from the Debian feed brings some dependencies that conflict
+# with Cog), and 2) Cog crashes with SEGFAULT on non-GPU devices.
+RUN if [ "$BUILD_TYPE" = "wayland" ]; then \
+    echo "deb http://deb.debian.org/debian experimental main" >> /etc/apt/sources.list && \
+    apt-get -y update && \
+    apt-get -t experimental install -y --no-install-recommends cog && \
+    apt-get clean && apt-get autoremove && rm -rf /var/lib/apt/lists/* ; fi
 
 # Copy the virtual keyboard extension from the first stage
 COPY --from=0 /extension/chrome-virtual-keyboard-master /chrome-extensions/chrome-virtual-keyboard
@@ -41,7 +58,6 @@ COPY start-browser.sh /usr/bin/start-browser
 
 USER torizon
 
-ENV LIBGL_ALWAYS_SOFTWARE=1
 ENV DISPLAY=:0
 
 ENTRYPOINT ["/usr/bin/start-browser"]
--- start-browser.sh
+++ start-browser.sh
@@ -1,38 +1,73 @@
 #!/bin/sh
 
-# default url
-url="www.toradex.com"
+# default URL
+KIOSK_URL="www.toradex.com"
 
-# default parms for kiosk mode
-chromium_parms_base="--test-type --allow-insecure-localhost --disable-notifications --check-for-update-interval=315360000 "
-chromium_parms="--kiosk "
-# Additional params should be stacked chromium_parms_extended="$chromium_parms_extended ..."
-chromium_parms_extended=""
-
-# FIXME: TOR-1426: work around seccomp-bpf failures in chromium.
-chromium_parms_extended="$chromium_parms_extended --disable-seccomp-filter-sandbox"
-
-for arg in "$@"
-do
-    case $arg in
-    --window-mode)
-        chromium_parms="--start-maximized --app="
-        shift
-        ;;
-    --browser-mode)
-        chromium_parms="--start-maximized "
-        shift
-        ;;
-    --virtual-keyboard)
-        # Load the virtual keyboard
-        chromium_parms_extended="$chromium_parms_extended --load-extension=/chrome-extensions/chrome-virtual-keyboard"
-        shift
-        ;;
-    esac
-done
-
-if [ ! -z "$1" ]; then
-    url=$1
+# LAUNCHER
+if [ -z "$KIOSK_LAUNCHER" ]; then
+    KIOSK_LAUNCHER="chromium"
 fi
 
-exec chromium $chromium_parms_base $chromium_parms_extended $chromium_parms$url
+start_chromium() {
+    KIOSK_ENV="LIBGL_ALWAYS_SOFTWARE=1 $KIOSK_ENV"
+
+    # default parms for kiosk mode
+    chromium_parms_base="--test-type --allow-insecure-localhost --disable-notifications --check-for-update-interval=315360000 "
+    chromium_parms="--kiosk "
+
+    # Additional params should be stacked chromium_parms_extended="$chromium_parms_extended ..."
+    chromium_parms_extended=""
+
+    # FIXME: TOR-1426: work around seccomp-bpf failures in chromium.
+    chromium_parms_extended="$chromium_parms_extended --disable-seccomp-filter-sandbox"
+
+    for arg in "$@"
+    do
+        case $arg in
+        --window-mode)
+            chromium_parms="--start-maximized --app="
+            shift
+            ;;
+        --browser-mode)
+            chromium_parms="--start-maximized "
+            shift
+            ;;
+        --virtual-keyboard)
+            # Load the virtual keyboard
+            chromium_parms_extended="$chromium_parms_extended --load-extension=/chrome-extensions/chrome-virtual-keyboard"
+            shift
+            ;;
+        esac
+    done
+
+    if [ ! -z "$1" ]; then
+        KIOSK_URL=$1
+    fi
+
+    eval $KIOSK_ENV exec chromium $chromium_parms_base $chromium_parms_extended $KIOSK_ARGS $chromium_parms$KIOSK_URL
+}
+
+start_cog() {
+    KIOSK_ENV="COG_PLATFORM_FDO_VIEW_FULLSCREEN=1 $KIOSK_ENV"
+    KIOSK_ARGS="-P fdo $KIOSK_ARGS"
+
+    if [ ! -z "$1" ]; then
+        KIOSK_URL=$1
+    fi
+
+    eval $KIOSK_ENV exec cog $KIOSK_ARGS $KIOSK_URL
+}
+
+case "$KIOSK_LAUNCHER" in
+    cog)
+        start_cog $@
+        ;;
+
+    chromium)
+        start_chromium $@
+        ;;
+
+    *)
+        echo "Launcher $KIOSK_LAUNCHER is invalid!"
+        exit 1
+esac
