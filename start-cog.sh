94e84c6ca09a6a68fab9323bfaa9700c35ecab1f
Dejan Jesenko <dejan.jesenko@toradex.com>
2022-04-26 18:17:31 +0200

    update Cog container to version 0.12.4-1
    Version 0.12.4-1 of Cog now supports also
    non-GPU devices (iMX7 and iMX6ULL).
    
    Related-to: TOR-2255
    
    Signed-off-by: Dejan Jesenko <dejan.jesenko@toradex.com>

--- start-cog.sh
+++ start-cog.sh
@@ -3,11 +3,8 @@
 # default URL
 URL="www.toradex.com"
 
-    KIOSK_ENV="COG_PLATFORM_FDO_VIEW_FULLSCREEN=1 $KIOSK_ENV"
-    KIOSK_ARGS="-P fdo $KIOSK_ARGS"
-
     if [ ! -z "$1" ]; then
         URL=$1
     fi
 
-    eval $KIOSK_ENV exec cog $KIOSK_ARGS $URL
+    exec cog $URL
